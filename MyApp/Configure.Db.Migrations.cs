b9f398d9ec04935532d9bbc33cdc8d87642769e2
Demis Bellot <demis.bellot@gmail.com>
2025-02-05 11:16:46 +0800

    Add support for AuditEvents

--- MyApp/Configure.Db.Migrations.cs
+++ MyApp/Configure.Db.Migrations.cs
@@ -42,7 +42,7 @@ public class ConfigureDbMigrations : IHostingStartup
             });
             AppTasks.Register("migrate.revert", args => migrator.Revert(args[0]));
             AppTasks.Register("migrate.rerun", args => migrator.Rerun(args[0]));
-            AppTasks.Register("App.json", args => // Default App.db 
+            AppTasks.Register("App.json", args => // Default App.db
                 appHost.VirtualFiles.WriteFile("App_Data/App.json", ClientConfig.ToSystemJson(
                     migrator.DbFactory.GetTables(namedConnection:null, schema:null))));
 
